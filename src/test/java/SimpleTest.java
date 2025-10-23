0f7ad5d7159adc6003f1d9baad62abe5fc09f22b
harry <harifosa@gmail.com>
2025-10-23 17:38:34 -0400

    Add JaCoCo coverage configuration and JUnit 3.8.1 compatible test

---
+++ src/test/java/SimpleTest.java
@@ -0,0 +1,9 @@
+import junit.framework.TestCase;
+
+public class SimpleTest extends TestCase {
+    
+    public void testBasic() {
+        assertTrue("Basic test", true);
+        assertEquals(4, 2 + 2);
+    }
+}
