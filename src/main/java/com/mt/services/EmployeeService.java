8e5068378faf18d8907c681b7efa137bff98a56c
harry <harifosa@gmail.com>
2025-10-23 17:52:16 -0400

    Fix SonarQube issues: use @RestController and @GetMapping

--- src/main/java/com/mt/services/EmployeeService.java
+++ src/main/java/com/mt/services/EmployeeService.java
@@ -6,18 +6,16 @@ import javax.servlet.http.HttpSession;
 
 import org.json.JSONException;
 import org.json.JSONObject;
-import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.RestController;
 import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RequestMethod;
-import org.springframework.web.bind.annotation.ResponseBody;
+import org.springframework.web.bind.annotation.GetMapping;
 
-@Controller
+@RestController
 @RequestMapping("/employee")
 public class EmployeeService {
 
 	
-	@RequestMapping(value = "/getEmployeeDetails", method = RequestMethod.GET)
-	@ResponseBody
+	@GetMapping("/getEmployeeDetails")
 	String uploadImage(HttpServletRequest request, HttpServletResponse response, HttpSession httpSession)
 			throws JSONException {
 
